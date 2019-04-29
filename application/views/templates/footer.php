68507c0e2942e42e0b605a138ffea34a1278f9e2
Fikhri <36291552+fikhrimasri@users.noreply.github.com>
2019-04-29 21:33:16 +0700

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,44 @@
+<div class="jumbotron bg-primary" style="margin-top:50px; margin-bottom: -25px">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="logokalibrr.png" width="100" style="margin-top:-50px; margin-bottom: 10px"></br>
+              <span class="text-light">We transform the way candidates find jobs and companies hire talent.</span>
+            </div>
+            <div class="col-md-2">
+              <a href="#"><span class="text-light">About</span></a> </br>
+              <a href="#"><span class="text-light">Careers</span></a> </br>
+              <a href="#"><span class="text-light">Internships</span></a> </br>
+              <a href="#"><span class="text-light">Press</span></a> </br>
+              <a href="#"><span class="text-light">Blog</span></a> </br>
+              <a href="#"><span class="text-light">Contact</span></a> </br>
+            </div>
+            <div class="col-md-2">
+              <h6 class="text-muted"style="margin-bottom:-20px">CANDIDATE</h6> </br>
+              <a href="#"><span class="text-light">Job Board</span></a> </br>
+              <a href="#"><span class="text-light">Career Advice</span></a> </br>
+              <a href="#"><span class="text-light">Help for Jobseekers</span></a> </br>
+              <a href="#"><span class="text-light">Jobseekeer Guide</span></a> </br>
+            </div>
+            <div class="col-md-2">
+                <h6 class="text-muted"style="margin-bottom:-20px">COMPANY</h6> </br>
+                <a href="#"><span class="text-light">Assessments</span></a> </br>
+                <a href="#"><span class="text-light">Pricing</span></a> </br>
+                <a href="#"><span class="text-light">Powered by Kalibrr</span></a> </br>
+                <a href="#"><span class="text-light">Help for Employers</span></a> </br>
+                <a href="#"><span class="text-light">Resources for Recruiters</span></a> </br>
+            </div>
+          </div>
+          <div class="card" style="height:70px ; margin-top: 50px; margin-bottom:15px">
+              <div class="card-body bg-primary"></div>
+          </div>
+          <span class="text-light" style="margin-right:8px; font-size: 14px">© 2019 Kalibrr, Inc.</span> 
+          <a href="#"><span class="text-light" style="margin-right:8px; font-size: 14px">Terms of Use</span></a> 
+          <a href="#"><span class="text-light" style="margin-right:8px; font-size: 14px">Data Sharing Policy</span></a> 
+          <a href="#"><span class="text-light" style="margin-right:8px; font-size: 14px">Privacy Policy</span></a> 
+          <a href="#"><span class="text-light" style="margin-right:8px; font-size: 14px">Report a problem</span></a> 
+        </div>
+
+
+
+    </div>      
---
+++ application/views/templates/header_signup.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap4.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/Login.css">
+    <title>Employers</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+    <div class="container">
+    <div class="col-3">
+        <a href="#"><img src="<?php echo base_url();?>assets/image/Kalibrr.png" width="100px"></a>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url();?>index.php/C_Auth/index">Jobseekers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" color="blue" href="<?php echo base_url();?>index.php/C_Auth/employee">Employers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="<?php echo base_url();?>index.php/C_Auth/bSignup">Sign Up</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="<?php echo base_url();?>index.php/C_Auth/Signin">Sign In</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                English
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">English</a>
+                <a class="dropdown-item" href="#">Bahasa (ID)</a>
+              </div>
+            </li>
+    </div>
+    </div>
+    </nav>
