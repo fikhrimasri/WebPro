7593671d67a46c772af79ab3a14c669382cf15e3
Fikhri <36291552+fikhrimasri@users.noreply.github.com>
2019-04-29 21:30:52 +0700

    Add files via upload
    View_Employers

---
+++ application/views/V_Employers.php
@@ -0,0 +1,190 @@
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="<?php echo base_url();?>assets/image/Latar1.jpg" alt="" height="500">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="<?php echo base_url();?>assets/image/Latar2.jpg" alt="" height="500">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="<?php echo base_url();?>assets/image/Latar3.jpg" alt="" height="500">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="container" style="padding-top:50px;padding-bottom:50px">
+    <div class="row">
+      <div class="col-md-2" >
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image/BCA.png" alt="">
+      </div>
+      <div class="col-md-2">
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image/Gojek.png" alt="">
+      </div>
+      <div class="col-md-2">
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image/Accenture.png" alt="">
+      </div>
+      <div class="col-md-2">
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image/pwc.png" alt="">
+      </div>
+      <div class="col-md-2">
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image//Shopee.png" alt="">
+      </div>
+      <div class="col-md-2">
+        <img class="d-block w-100" src="<?php echo base_url();?>assets/image/unilever.png" alt="">
+      </div>
+      </div>
+    </div>
+
+  
+<!-- <footer class="page-footer font-small mdb-color pt-4">
+
+    
+    <div class="container text-center text-md-left">
+
+      
+      <div class="row text-center text-md-left mt-3 pb-3">
+
+        
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
+          <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.</p>
+        </div>
+       
+
+        <hr class="w-100 clearfix d-md-none">
+
+        
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+          <p>
+            <a href="#!">MDBootstrap</a>
+          </p>
+          <p>
+            <a href="#!">MDWordPress</a>
+          </p>
+          <p>
+            <a href="#!">BrandFlow</a>
+          </p>
+          <p>
+            <a href="#!">Bootstrap Angular</a>
+          </p>
+        </div>
+        
+
+        <hr class="w-100 clearfix d-md-none">
+
+        
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+        </div>
+
+        
+        <hr class="w-100 clearfix d-md-none">
+
+        
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p>
+            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+        
+
+      </div>
+      
+
+      <hr>
+
+      
+      <div class="row d-flex align-items-center">
+
+        
+        <div class="col-md-7 col-lg-8">
+
+          
+          <p class="text-center text-md-left">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/">
+              <strong> MDBootstrap.com</strong>
+            </a>
+          </p>
+
+        </div>
+        
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+          
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-google-plus-g"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        
+
+      </div>
+      
+
+    </div>
+    
+
+  </footer> -->
+  <!-- Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
